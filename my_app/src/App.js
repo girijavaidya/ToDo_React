cf645ab5490bc9f86e09b2fb02671c1f0a07f817
Girija Vaidya <vaidyagirija58@gmail.com>
2025-10-24 14:12:41 +0530

    Normalize line endings

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,38 +1,42 @@
-
 import './App.css';
 
 function App() {
   return (
-    <div className="App d-flex">
-    <h1 className='fs-1 text-center'>TO-DO APP</h1>
+    <div className="App">
+      <h1 className="fs-1 text-center">TO-DO APP</h1>
 
-    <div className='todo-wrapper d-flex d-row justify-content-center align-item-center'>
-      <div className='todo-input'>
-        <label>Title</label>
-        <input type="text" placeholder='Enter the Title'/>
-      </div>
-      <div className='todo-input'>
-          <label>Discription</label>
-        <input type="text" placeholder='Enter the Title-Discription'/>
-      </div>
-      <div className='todo-input'>
-         <button type='button' className='primaryBtn'>Add</button>
-       
-      </div>
- <div className='btn-area'>
-        <button>ToDo</button>
+      <div className="todo-wrapper d-flex flex-column justify-content-center align-items-center">
+        {/* Input Section */}
+        <div className="d-flex todo-input-group">
+          <div className="todo-input">
+            <label>Title</label>
+            <input type="text" placeholder="Enter the Title" />
+          </div>
+
+          <div className="todo-input">
+            <label>Description</label>
+            <input type="text" placeholder="Enter the Title Description" />
+          </div>
+
+          <div className="todo-input">
+            <button type="button" className="primaryBtn">Add</button>
+          </div>
+        </div>
+
+        {/* Buttons Section */}
+        <div className="btn-area mt-3">
+          <button>ToDo</button>
           <button>Completed</button>
-    </div>
-    <div className='ToDO List'>
-       <div className='todo-list item'>
-        <h3>Task1</h3>
-        <p>Description</p>
-       </div>
-    </div>
-    </div>
-   
+        </div>
 
-    
+        {/* Task List Section */}
+        <div className="todo-list mt-3">
+          <div className="todo-list-item">
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
