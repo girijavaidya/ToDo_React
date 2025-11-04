687a16226cc57f3b78a86916fb3572df551dd44e
Girija Vaidya <vaidyagirija58@gmail.com>
2025-11-04 14:09:25 +0530

    To-DoList in React

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,43 +1,109 @@
+import React, { useState } from 'react';
+
+
 import './App.css';
 
+import { FaTrash, FaCheck } from "react-icons/fa"; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  const [allTodos, setTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewdescription] = useState("");
+
+  const handleAddTool = ()=>
+  {
+    let newTodoitem =
+    {
+      title:newTitle,
+      description:newDescription
+    }
+
+    let updatedTodoArr = 
+    [
+      ...allTodos
+    ];
+    updatedTodoArr.push(newTodoitem);
+    setTodos(updatedTodoArr)
+  }
+
   return (
     <div className="App">
-      <h1 className="fs-1 text-center">TO-DO APP</h1>
+      <div>
+      <h1 className="heading fs-1 text-center">TO-DO APP</h1>
+    </div>
 
       <div className="todo-wrapper d-flex flex-column justify-content-center align-items-center">
         {/* Input Section */}
-        <div className="d-flex todo-input-group">
-          <div className="todo-input">
-            <label>Title</label>
-            <input type="text" placeholder="Enter the Title" />
-          </div>
+     <div className="d-flex align-items-end justify-content-start gap-3 flex-wrap todo-input-group">
 
-          <div className="todo-input">
-            <label>Description</label>
-            <input type="text" placeholder="Enter the Title Description" />
-          </div>
+  {/* Title Input */}
+  <div className="todo-input">
+    <label className="form-label mb-1">Title</label>
+    <input type="text" className="form-control" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder="Enter the Title" />
+  </div>
 
-          <div className="todo-input">
-            <button type="button" className="primaryBtn">Add</button>
-          </div>
-        </div>
+  {/* Description Input */}
+  <div className="todo-input">
+    <label className="form-label mb-1">Description</label>
+    <input type="text" value={newDescription} onChange={(e)=>setNewdescription(e.target.value)} className="form-control"  placeholder="Enter the Title Description" />
+  </div>
 
-        {/* Buttons Section */}
-        <div className="btn-area mt-3">
-          <button>ToDo</button>
-          <button>Completed</button>
-        </div>
+  {/* Buttons */}
+  <div class="todo-input d-flex  gap-2 flex-nowrap">
+  <button type="button" class=" primaryBtn" onClick={handleAddTool}>Add</button>
 
-        {/* Task List Section */}
-        <div className="todo-list mt-3">
-          <div className="todo-list-item">
-            <h3>Task 1</h3>
-            <p>Description</p>
-          </div>
-        </div>
+ 
+</div>
+
+</div>
+
+
+        
+        <div className="container-fluid">
+  {/* <div className="row"> */}
+    <div className="col-lg-8 col-md-10 col-12"> {/* controls width and left alignment */}
+      
+      {/* Button Section */}
+      <div className="btn-area py-4">
+        <button  className={`secondaryBtn ${isCompleteScreen === false ? "active" : ""}`} onClick={()=>setIsCompleteScreen(false)}>ToDo</button>
+        <button className={`secondaryBtn ${isCompleteScreen === true ? "active" : ""}`}onClick={()=>setIsCompleteScreen(true)}>Completed</button>
       </div>
+
+      {/* Task List Section */}
+      <div className="todo-list">
+                
+     {allTodos.map((item, index) => {
+  return (
+    <div key={index}>
+      {item}
     </div>
+  );
+})}
+
+
+        {/* <div className="todo-list-item d-flex align-items-center justify-content-between p-3  mb-3  shadow-sm">
+          
+          <div className="text-section">
+            <h1 className="mb-1 fs-2 text-white">Task 1</h1>
+            <p className="mb-0 " style={{color: "rgb(161, 161, 161)"}} >Description</p>
+          </div>
+
+          <div className="todo-icons d-flex gap-3">
+            <FaCheck className="text-success fs-2" style={{ cursor: "pointer" }} />
+            <FaTrash className="text-white fs-2" style={{ cursor: "pointer" }} />
+          </div>
+        </div> */}
+      </div>
+
+    </div>
+  </div>
+</div>
+
+      </div>
+   
   );
 }
 
