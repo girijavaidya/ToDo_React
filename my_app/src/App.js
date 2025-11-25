7f7a2bd95fb4e2d5d7bcdd7603df74f1120431a4
Girija Vaidya <vaidyagirija58@gmail.com>
2025-11-25 23:33:00 +0530

    Adding and delete functionality

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -14,6 +14,7 @@ function App() {
   const [allTodos, setTodos] = useState([]);
   const [newTitle, setNewTitle] = useState("");
   const [newDescription, setNewdescription] = useState("");
+  const [completedTodos, setCompletedTodos] = useState([]);
 
 
 
@@ -40,14 +41,41 @@ function App() {
   };
 
   // delete todo item
-  const handleDeleteTodo = (index)=>
+const handleDeleteTodo = (index) => {
+  setTodos(prevTodos => {
+    const updated = prevTodos.filter((_, i) => i !== index);
+    localStorage.setItem("todolist", JSON.stringify(updated));
+    return updated;
+  });
+};  
+
+// complete todo item
+const handleCompleted = (index)=>
+{
+  let now = new Date();
+  let dd = now.getDate();
+  let mm = now.getMonth() + 1;
+  let yyyy = now.getFullYear();
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let s = now.getSeconds();
+
+  let completedOn = dd +'-'+ mm +'-' + yyyy + 'at' + h + ':'+ m+ ':'  + s;
+
+  let filteredItem = 
   {
-    let reducedTodo = [...allTodos]
-    // remaning the array
-    reducedTodo.splice(index);
-    localStorage.setItem('todolist',JSON.stringify(reducedTodo) )
-    setTodos(reducedTodo)
+    ...allTodos[index],
+    completedOn:completedOn
   }
+    let updatedCompletedArr = [...completedTodos]; 
+    updatedCompletedArr.push(filteredItem);
+    setCompletedTodos(updatedCompletedArr);
+    handleDeleteTodo(index)
+  }
+
+
+
+
 
   // useEffect use it
 // useeffect use that when ever use render the component
@@ -57,7 +85,7 @@ function App() {
 // create variable getting the data from local storage
 // when you convert into array or object to backend use JSON.paras
 // JSON.paras is convert into localstorage data into the array
-    let savedTodos = JSON.parse(localStorage.getItem('todolist'))
+    let savedTodos = JSON.parse(localStorage.getItem("todolist"))
 
     if(savedTodos)
     {
@@ -76,27 +104,28 @@ function App() {
 
       <div className="todo-wrapper d-flex flex-column justify-content-center align-items-center">
         {/* Input Section */}
+        <div className="todo-input-container">
      <div className="d-flex align-items-end justify-content-start gap-3 flex-wrap todo-input-group">
 
   {/* Title Input */}
   <div className="todo-input">
     <label className="form-label mb-1">Title</label>
-    <input type="text" className="form-control" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder="Enter the Title" />
+    <input type="text" className="form-control" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder="Enter the Title" required/>
   </div>
 
   {/* Description Input */}
   <div className="todo-input">
     <label className="form-label mb-1">Description</label>
-    <input type="text" value={newDescription} onChange={(e)=>setNewdescription(e.target.value)} className="form-control"  placeholder="Enter the Title Description" />
+    <input type="text" value={newDescription} onChange={(e)=>setNewdescription(e.target.value)} className="form-control"  placeholder="Enter the Title Description" required/>
   </div>
 
   {/* Buttons */}
-  <div class="todo-input d-flex  gap-2 flex-nowrap">
+  <div className="todo-input d-flex  gap-2 flex-nowrap">
   <button type="button" class=" primaryBtn" onClick={handleAddTool}>Add</button>
 
  
 </div>
-
+</div>
 </div>
 
 
@@ -114,18 +143,47 @@ function App() {
       {/* Task List Section */}
       <div className="todo-list">
                 
-     {allTodos.map((item, index) => {
+     {isCompleteScreen===false && allTodos.map((item, index) => {
   return (
     <div className="todo-list-item d-flex align-items-center justify-content-between p-3  mb-3  shadow-sm" key={index}>
           
           <div className="text-section">
-            <h1 className="mb-1 fs-4 text-white">{item.title}</h1>
+            <h1 className="mb-1 fs-4 text-danger">{item.title}</h1>
             <p className="mb-0 " style={{color: "rgb(161, 161, 161)"}}>{item.description}</p>
           </div>
 
           <div className="todo-icons d-flex gap-3">
-            <FaCheck className="text-success fs-5 " onClick={()=>handleDeleteTodo(index)} style={{ cursor: "pointer" }} />
-            <FaTrash className=" fs-5 text-danger" style={{ cursor: "pointer" }} />
+         <FaCheck
+  className="text-success fs-5"
+ onClick={()=>handleCompleted(index)}
+  style={{ cursor: "pointer" }}
+/>
+
+            <FaTrash className=" fs-5 text-danger"  onClick={() => handleDeleteTodo(index)} style={{ cursor: "pointer" }} />
+          </div>
+        </div> 
+    
+  );
+})}
+
+{isCompleteScreen===true && completedTodos.map((item, index) => {
+  return (
+    <div className="todo-list-item d-flex align-items-center justify-content-between p-3  mb-3  shadow-sm" key={index}>
+          
+          <div className="text-section">
+            <h1 className="mb-1 fs-4 text-danger">{item.title}</h1>
+            <p className="mb-0 " style={{color: "rgb(161, 161, 161)"}}>{item.description}</p>
+            <p><small>Completed On: {item.completedOn}</small></p>
+          </div>
+
+          <div className="todo-icons d-flex gap-3">
+         {/* <FaCheck
+  className="text-success fs-5"
+ onClick={()=>handleCompleted(index)}
+  style={{ cursor: "pointer" }}
+/> */}
+
+            <FaTrash className=" fs-5 text-danger"  onClick={() => handleDeleteTodo(index)} style={{ cursor: "pointer" }} />
           </div>
         </div> 
     
@@ -133,18 +191,8 @@ function App() {
 })}
 
 
-      {/* <div className="todo-list-item d-flex align-items-center justify-content-between p-3  mb-3  shadow-sm">
-          
-          <div className="text-section">
-            <h1 className="mb-1 fs-4 text-white">Task 1</h1>
-            <p className="mb-0 " style={{color: "rgb(161, 161, 161)"}} >Description</p>
-          </div>
 
-          <div className="todo-icons d-flex gap-3">
-            <FaCheck className="text-success fs-5 " style={{ cursor: "pointer" }} />
-            <FaTrash className=" fs-5 text-danger" style={{ cursor: "pointer" }} />
-          </div>
-        </div>  */}
+      
       </div>
 
     </div>
